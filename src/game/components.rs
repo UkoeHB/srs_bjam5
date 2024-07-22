3f01517fbaa225ba32bd786bbd0a5a56b1ab45ed
Aura <45722265+auralluring@users.noreply.github.com>
2024-07-21 21:11:37 -0600

    add Health component

--- src/game/components.rs
+++ src/game/components.rs
@@ -11,3 +11,26 @@ use bevy::prelude::*;
 pub struct Barrier;
 
 //-------------------------------------------------------------------------------------------------------------------
+
+#[derive(Debug, Clone, Copy, Event)]
+pub struct EntityDeath;
+
+//-------------------------------------------------------------------------------------------------------------------
+
+#[derive(Debug, Component)]
+pub struct Health {
+    pub current: usize,
+    pub max: usize,
+}
+
+impl Health {
+    pub fn from_max(max: usize) -> Self {
+        Self { current: max, max }
+    }
+
+    pub fn set_health(&mut self, new: usize) {
+        self.current = new.min(self.max)
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------------
