9c2730f996ebed71a36ccda30e7f8c81b3250bed
koe <ukoe@protonmail.com>
2024-07-23 14:20:07 -0500

    remove light spot hack from bevy_lit

--- src/game/light.rs
+++ src/game/light.rs
@@ -6,22 +6,6 @@ use crate::*;
 
 //-------------------------------------------------------------------------------------------------------------------
 
-//todo: remove this once ambient light fixed upstream
-fn setup_light_hack(mut c: Commands)
-{
-    c.spawn((PointLight2dBundle {
-        point_light: PointLight2d {
-            intensity: 0.0,
-            radius: 0.0,
-            falloff: 0.0,
-            color: Color::WHITE,
-        },
-        ..default()
-    },));
-}
-
-//-------------------------------------------------------------------------------------------------------------------
-
 fn update_light(mut light: ResMut<AmbientLight2d>, clock: Res<GameClock>, constants: ReactRes<GameConstants>)
 {
     let day_progress = clock.elapsed.as_secs_f32() / constants.day_length_secs as f32;
@@ -40,7 +24,6 @@ impl Plugin for LightPlugin
     fn build(&self, app: &mut App)
     {
         app.add_plugins(Lighting2dPlugin::default())
-            .add_systems(Startup, setup_light_hack)
             .add_systems(Update, update_light.run_if(in_state(PlayState::Day)));
     }
 }
