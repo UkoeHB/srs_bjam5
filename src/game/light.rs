c317e7769d39341740aa471389527e5f1bb4170d
Aura <45722265+auralluring@users.noreply.github.com>
2024-07-23 01:04:32 -0600

    use a parabola to calculate daylight level

--- src/game/light.rs
+++ src/game/light.rs
@@ -11,10 +11,7 @@ impl Plugin for LightPlugin
 {
     fn build(&self, app: &mut App)
     {
-        app.add_plugins(Lighting2dPlugin {
-            ambient_light: AmbientLight2d { brightness: 0.2, color: Color::Srgba(WHITE) },
-            ..Default::default()
-        });
+        app.add_plugins(Lighting2dPlugin::default());
         app.add_systems(OnEnter(GameState::Play), setup_light);
         app.add_systems(Update, update_light.run_if(in_state(GameState::Play)));
     }
@@ -36,5 +33,9 @@ fn setup_light(mut c: Commands)
 fn update_light(mut light: ResMut<AmbientLight2d>, clock: Res<GameClock>, constants: ReactRes<GameConstants>)
 {
     let day_progress = clock.elapsed_secs() as f32 / constants.day_length_secs as f32;
-    light.brightness = 1.0 - day_progress;
+
+    // get brighter until halfway through the day, then start going down
+    // parabolas are fun sometimes i guess. desmos graph i used for tweaking: https://www.desmos.com/calculator/jxilotpz1s
+    light.brightness = (-2.3 * day_progress * day_progress) + (2. * day_progress) + 0.5;
+    println!("{}, {}", day_progress, light.brightness);
 }
